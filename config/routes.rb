1f7f21fe605f9e4d618c49027b3ccc355c152a3c
West0ne <copiapoa12@gmail.com>
2017-03-06 22:54:29 +0200

    add new app

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help', as: 'help'
+  get  '/about',   to: 'static_pages#about', as: 'about'
+  get  '/contact', to: 'static_pages#contact', as: 'contact'
+end
